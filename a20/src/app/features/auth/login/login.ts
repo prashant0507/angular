4272cd4837494bd075d97735da3ce31e7e7dafa5
Prashant <shrivastavaprashant91@gmail.com>
2026-05-29 01:28:40 +0530

    setup angular material and create form for login with email validation

--- a20/src/app/features/auth/login/login.ts
+++ a20/src/app/features/auth/login/login.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
+export class Login implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  errorMessage = signal('');
+  login!: FormGroup;
+  ngOnInit(): void {
+    this.login = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
+  onSubmit(){
+    console.log(this.login.value);
+  }
+
+  updateErrorMessage() {
+    if (this.login?.get('email')?.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.login?.get('email')?.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 }
