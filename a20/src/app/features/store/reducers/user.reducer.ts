a39bb14532e17bb8623301c5953a871fa55008a3
Prashant <shrivastavaprashant91@gmail.com>
2026-06-03 01:06:38 +0530

    create action reducer selector and effect by commands

---
+++ a20/src/app/features/store/reducers/user.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer, on } from '@ngrx/store';
+
+export const userFeatureKey = 'user';
+
+export interface State {
+
+}
+
+export const initialState: State = {
+
+};
+
+export const reducer = createReducer(
+  initialState,
+);
+
